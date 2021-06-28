87193eacacd389124454d851c2b44be160be0237
HannanAkhtar <51832669+HannanAkhtar@users.noreply.github.com>
2021-06-29 02:23:26 +0400

    Add files via upload

--- application/config.py
+++ application/config.py
@@ -9,4 +9,3 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
